1ef38474a4ae2459e35cf8f442f44b0854e35b33
aaronekenlDev <87204069+aaronekenlDev@users.noreply.github.com>
2021-07-10 23:47:20 +0200

    Add files via upload

---
+++ bin/joke.js
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+const axios = require("axios");
+console.log('Alright.. let me fetch a joke.. woof');
+
+
+const url = "https://icanhazdadjoke.com/";
+axios.get(url, { headers: { Accept: "application/json" } })
+ .then(res => {
+   console.log(res.data.joke);
+ });
